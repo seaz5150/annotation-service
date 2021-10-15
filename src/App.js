60dd8e2c5daed2b7770bdc0d8fd4e7d8179362c5
seaz5150 <jan.plhal1@gmail.com>
2021-10-15 21:20:43 +0200

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <Dashboard />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+// const [name, setName] = useState("mario");
+// setName("luigi");
+
+const Dashboard = () => {
+    return (  
+        <div className="dashboard">
+
+        </div>
+    );
+}
+
+export default Dashboard;
