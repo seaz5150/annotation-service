0b677d81b340e5133f1ed4b5cde57d77c5de4cb5
Jan Plhal <jan.plhal1@gmail.com>
2021-12-18 12:48:34 +0100

    Added segment color enum.

---
+++ src/SegmentColors.tsx
@@ -0,0 +1,29 @@
+//https://wondernote.org/color-palettes-for-web-digital-blog-graphic-design-with-hexadecimal-codes/
+
+export const enum SegmentColors {
+    Color1 = "#abdee6",
+    Color2 = "#cbaacb",
+    Color3 = "#ffffb5",
+    Color4 = "#ffccb6",
+    Color5 = "#f3b0c3",
+    Color6 = "#c6dbda",
+    Color7 = "#fee1e8",
+    Color8 = "#fed7c3",
+    Color9 = "#f6eac2",
+    Color10 = "#ecd5e3",
+    Color11 = "#ff968a",
+    Color12 = "#ffaea5",
+    Color13 = "#ffc5bf",
+    Color14 = "#ffd8be",
+    Color15 = "#ffc8a2",
+    Color16 = "#d4f0f0",
+    Color17 = "#8fcaca",
+    Color18 = "#cce2cb",
+    Color19 = "#b6cfb6",
+    Color20 = "#97c1a9",
+    Color21 = "#fcb9aa",
+    Color22 = "#ffdbcc",
+    Color23 = "#eceae4",
+    Color24 = "#a2e1db",
+    Color25 = "#55cbcd"
+}
