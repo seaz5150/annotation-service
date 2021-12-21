7bd1142af17ed40f267355625441ea5ebf7ab0d5
Jan Plhal <jan.plhal1@gmail.com>
2021-12-21 15:25:48 +0100

    Info message on 0 segments.

--- src/components/AnnotationText.tsx
+++ src/components/AnnotationText.tsx
@@ -565,9 +565,18 @@ export default function AnnotationText() {
     return (
     <div className="segments">
         {segments &&
-            segments.map((segment: any) => 
-                <AnnotationSegment segmentId={segment.id} key={segment.id}/>
-            )
+            <React.Fragment>
+                {(segments.length > 0) ?
+                    segments.map((segment: any) => 
+                        <AnnotationSegment segmentId={segment.id} key={segment.id}/>
+                    )
+                    :
+                    <div className="card card-body module module-content segment segment-placeholder">
+                        <i className="bi bi-info-circle me-2 info-icon"></i>
+                        Create a caption by dragging on the waveform.
+                    </div>
+                }
+            </React.Fragment>
         }
     </div>
     );
