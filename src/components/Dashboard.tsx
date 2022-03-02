e17f054a41a782eb49ea16ce156936f33210089f
Jan Plhal <jan.plhal1@gmail.com>
2022-03-02 16:23:54 +0100

    Dynamic grid element size WIP.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -9,6 +9,7 @@ import AnnotationText from "./AnnotationText"
 import RecordingDetails from "./RecordingDetails"
 
 import React, {
+    useEffect,
     useRef, useState,
   } from "react";
 
@@ -28,12 +29,51 @@ function Dashboard({ size: { width } }: {size: SizeParams})
     //     setItems([...modules, moduleId]);
     // };
 
+    const [audioPlayerDimensions, setAudioPlayerDimensions] = useState({width: 12, height: 9});
+    const [annotationTextDimensions, setAnnotationTextDimensions] = useState({width: 6, height: 5});
+
+    const moduleRefs = useRef([] as any[]); 
+    moduleRefs.current = [];
+
+    const addToModuleRefs = (moduleEl: any, moduleName: any) => {
+        if (moduleEl && !moduleRefs.current.includes(moduleEl)) {
+            moduleRefs.current.push({
+                moduleName: moduleName,
+                ref: moduleEl
+            });
+        }
+    }
+
+    useEffect(() => {
+        for (var index in moduleRefs.current) {
+            console.log("LOL")
+            var moduleRef = moduleRefs.current[index];
+
+            switch (moduleRef.moduleName) {
+                case "AudioPlayer":
+                    // var newWidth = Math.floor(moduleRef.ref.clientWidth / 123);
+                    // var newHeight = Math.floor(moduleRef.ref.clientHeight / 17);
+                    // setAudioPlayerDimensions({width: newWidth, height: newHeight})
+                    break;
+                case "AnnotationText":
+                    console.log("¨setting anno")
+                    //var newWidth = Math.floor((moduleRef.ref.scrollWidth + 10) / 20);
+                    var newHeight = Math.floor((moduleRef.ref. + 10) / 20);
+                    console.log(moduleRef.ref.clientHeight);
+                    setAnnotationTextDimensions({width: 6, height: newHeight})
+                    break;
+                default:
+                    break;
+            }
+        }
+    }, [moduleRefs.current.length]);
+
     const layouts = {
     lg: [
-        { i: 'AudioPlayer', x: 0, y: 0, w: 12, h: 9, isResizable: false},
-        { i: 'AnnotationText', x: 3, y: 0, w: 6, h: 5, isResizable: false},
+        { i: 'AudioPlayer', x: 0, y: 0, w: audioPlayerDimensions.width, h: audioPlayerDimensions.height, isResizable: false},
+        { i: 'AnnotationText', x: 3, y: 0, w: annotationTextDimensions.width, h: annotationTextDimensions.height, isResizable: false},
         { i: 'Settings', x: 0, y: 0, w: 2.7, h: 5, isResizable: false},
-        { i: 'RecordingDetails', x: 10, y: 0, w: 2.7, h: 10, isResizable: false},
+        { i: 'RecordingDetails', x: 10, y: 0, w: 2.7, h: 11.4, isResizable: false},
     ],
     };
 
@@ -50,16 +90,16 @@ function Dashboard({ size: { width } }: {size: SizeParams})
         // originalModules={originalModules}
     >
         <div key="AudioPlayer">
-            <AudioPlayer />
+            <AudioPlayer moduleRef={(el: any) => addToModuleRefs(el, "AudioPlayer")}/>
         </div>
         <div key="AnnotationText">
-            <AnnotationText />
+            <AnnotationText moduleRef={(el: any) => addToModuleRefs(el, "AnnotationText")}/>
         </div>
         {/* <div key="Settings">
             <Settings />
         </div> */}
         <div key="RecordingDetails">
-            <RecordingDetails />
+            <RecordingDetails moduleRef={(el: any) => addToModuleRefs(el, "RecordingDetails")}/>
         </div>
     </ResponsiveGridLayout>
     );
