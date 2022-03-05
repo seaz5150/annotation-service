90462f2f8fdc2a09c039a8507bf576a48939893f
seaz5150 <jan.plhal1@gmail.com>
2022-03-05 14:39:50 +0100

    Nested tag view.

--- src/components/AnnotationEditor.tsx
+++ src/components/AnnotationEditor.tsx
@@ -1,74 +1,121 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useCallback, useEffect, useMemo, useState } from "react";
 import { pressStopPropagation, recordingJson } from "../CommonUtilities";
 import { v4 as uuidv4 } from "uuid";
+import { createEditor, BaseEditor, Descendant, Selection, Transforms, Editor } from 'slate'
+import { Slate, Editable, withReact, ReactEditor } from 'slate-react'
+import Leaf from "slate-react/dist/components/leaf";
 
-interface EditorEntity {
-    id: string,
-    text: string,
-    tags?: Array<string>
+// interface EditorEntity {
+//     id: string,
+//     text: string,
+//     tags?: Array<string>
+// }
+
+
+export type TagElement = {
+  type: any
+  tagLabels?: string[]
+  children: any
+}
+
+type CustomText = { text: string }
+
+export type CustomElement = TagElement;
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
 }
 
 const AnnotationEditor = () => {
-    const [entities, setEntities] = useState<Array<EditorEntity>>();
-    let editorId = uuidv4();
+  const initialValue: Descendant[] = [
+    {
+      type: 'paragraph',
+      children: [
+        { text: 'This is editable ' },
+        { text: 'rich', tagLabels: ["first", "second", "third"] },
+        { text: ' text, ' },
+        { text: 'much', tagLabels: ["second"]},
+        { text: ' better than a ' },
+        { text: '<textarea>', tagLabels: ["first"] },
+        { text: '!' },
+      ],
+    },
+  ]
 
-    useEffect(() => {
-        let newEntities = Array<EditorEntity>();
-        for (let index in recordingJson.transcript.segments[0].words) {
-            let word: any = recordingJson.transcript.segments[0].words[index];
-            let newEntity = {id: uuidv4(), text: word.label, tags: (word.textTags ? word.textTags : Array<string>())} as EditorEntity;
-            newEntities.push(newEntity);
-        }
-        setEntities(newEntities);
-    }, []);
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState<Descendant[]>(initialValue);
 
-    const reformatEntities = () => {
-        console.log(window.getSelection()?.getRangeAt(0).cloneContents());
-        let selector = '[data-editor-id="' + editorId + '"]';
-        let editorElement = document.querySelector(selector);
-        if (editorElement === null) return;
+  let tagColors: { [name: string]: string } = {};
+  tagColors["first"] = "red";
+  tagColors["second"] = "green";
+  tagColors["third"] = "yellow";
 
-        let newEntities = Array<EditorEntity>();
-        let childNodes = Array.from(editorElement.childNodes);
-        for (let i in childNodes) {
-            let childNode = childNodes[i];
-            let textContent = childNode.textContent;
+  // const [entities, setEntities] = useState<Array<EditorEntity>>();
+  // useEffect(() => {
+  //     let newEntities = Array<EditorEntity>();
+  //     for (let index in recordingJson.transcript.segments[0].words) {
+  //         let word: any = recordingJson.transcript.segments[0].words[index];
+  //         let newEntity = {id: uuidv4(), text: word.label, tags: (word.textTags ? word.textTags : Array<string>())} as EditorEntity;
+  //         newEntities.push(newEntity);
+  //     }
+  //     setEntities(newEntities);
+  // }, []);
 
-            let words = textContent?.split(" ");
-            words = words?.map(word => word.trim());
-            words = words?.filter(word => word.trim().length > 0);
-
-            for (let j in words) {
-                let word = words[Number(j)];
-                let newEntity = {id: uuidv4(), text: word} as EditorEntity;
-
-                newEntities.push(newEntity);
-            }
-        }
-        console.log(newEntities);
-        setEntities(newEntities);
+  const renderElement = useCallback(({ attributes, children, element }) => {
+    switch (element.type) {
+      case "tag":
+        let paddingPx = children.length * 2;
+        const tagStyle = {
+          backgroundColor: element.tagLabel,
+          padding: paddingPx + "px",
+          borderRadius: "0.2em"
+        };
+        return <span style={tagStyle} {...attributes}>{children}</span>
+      default:
+        return <span {...attributes}>{children}</span>
     }
+  }, []);
 
-    return (  
-    <div className="segment-text"
-         data-editor-id={editorId}
-         contentEditable="true"
-         suppressContentEditableWarning={true}
-         onBlur={reformatEntities}
-         onMouseDown={e => pressStopPropagation(e)}>
-                {entities &&
-                    entities.map((entity: EditorEntity) => 
-                        entity.tags && entity.tags?.length > 0 
-                        ?
-                        <mark style={{backgroundColor: '#84d2ff', padding: '0 4px'}}>
-                            <span key={entity.id} data-id={entity.id} data-tags={entity.tags}>{entity.text} </span>
-                        </mark>
-                        : 
-                        <span key={entity.id} data-id={entity.id}>{entity.text} </span>
-                )}
-    </div>
-    );
+  const Leaf = useCallback(({ attributes, children, leaf }) => {
+    let tagLabels = leaf.tagLabels;
+    if (tagLabels) {
+      for (let index in tagLabels) {
+        let currentTagLabel = tagLabels[index];
+        let padding = 2 + Number(index);
+        children = <span style={{backgroundColor: tagColors[currentTagLabel], padding: padding + "px", borderRadius: "0.2em"}}>{children}</span>
+      }
+    }
+    return <span {...attributes}>{children}</span>
+  }, []);
+
+  const getSelection = () => {
+    let selection = editor.selection;
+    // let selected;
+    // console.log("lul");
+    // if (selection !== null && selection.anchor !== null) {
+    //   console.log(selection.anchor)
+    //   console.log(editor.children);
+    //   selected = editor.children[selection.anchor.path[0]];
+    // } else {
+    //   selected = null;
+    // }
+    // console.log(selected);
+    //Editor.addMarkAtRange()
+  };
+
+  return (
+    <>
+      <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
+        <Editable onMouseDown={pressStopPropagation} renderLeaf={Leaf}/>
+      </Slate>
+      <button onMouseDown={getSelection}>Style</button>
+      <pre>{JSON.stringify(value, null, 2)}</pre>
+    </>
+  );
 }
 
 export default AnnotationEditor;
-
