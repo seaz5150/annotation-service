80cacfa2daf5114556f06487a2701391c515da38
seaz5150 <jan.plhal1@gmail.com>
2022-03-16 12:27:20 +0100

    Add.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dashboard from './components/Dashboard';
+import SettingsWindow from './components/SettingsWindow';
 
 function App() {
+  const [settingsExpanded, setSettingsExpanded] = useState(false);
+
   return (
     <div className="App">
       <Dashboard />
+      <button className="settings-button strip-button-style" onClick={() => setSettingsExpanded(!settingsExpanded)}>
+        <i className="fas fa-cog"></i>
+      </button>
+      <SettingsWindow settingsExpanded={settingsExpanded} />
     </div>
   );
 }
