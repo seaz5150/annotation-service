e71fa3fcb54f9a0b1ca99533e8c52e74e40cd7a3
seaz5150 <jan.plhal1@gmail.com>
2022-04-18 11:25:28 +0200

    Slight margin change.

--- src/components/RecordingDetails.tsx
+++ src/components/RecordingDetails.tsx
@@ -61,7 +61,7 @@ const RecordingDetails = (props: RecordingDetailsInterface) => {
                     {jobData.user_interface.views.map((v: any) => v.type === "text" &&
                         <React.Fragment key={v.title}>
                             <p className="title-small col-10 ms-2 fw-normal">{v.title}</p>
-                            <input className="form-check-input custom-checkbox col-2 ms-3" 
+                            <input className="form-check-input custom-checkbox col-2 ms-4" 
                                     type="checkbox"
                                     onMouseDown={e => pressStopPropagation(e)} 
                                     onChange={(e) => toggleModule(e, v.title)}
@@ -71,7 +71,7 @@ const RecordingDetails = (props: RecordingDetailsInterface) => {
                     {jobData.user_interface.views.some((v: any) => v.type === "img" || v.type === "iframe") &&
                         <React.Fragment>
                             <p className="title-small col-10 ms-2 fw-normal">Visual resources (images, maps...)</p>
-                            <input className="form-check-input custom-checkbox col-2 ms-3" 
+                            <input className="form-check-input custom-checkbox col-2 ms-4" 
                                     type="checkbox"
                                     onMouseDown={e => pressStopPropagation(e)} 
                                     onChange={(e) => toggleModule(e, "AttachmentTabs")}
