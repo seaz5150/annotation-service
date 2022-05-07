8a1a2775287c869a6d7192629f50327213757971
seaz5150 <jan.plhal1@gmail.com>
2022-05-07 17:43:15 +0200

    Minor ApiRequests refactor.

--- src/utils/ApiRequests.ts
+++ src/utils/ApiRequests.ts
@@ -1,29 +1,31 @@
 import axios from "axios";
 
 const apiToken = "tyksc1fgc3jj5x6oc1wmxj3pabo74imfzpt8ittj";
+const path = "https://api.spokendata.com/v2/";
 
 export const createHttpsRequest = async (
-    config: any,
-    endpoint: string,
-    method: "POST" | "DELETE" | "GET" | "PATCH" | "PUT"
-  ) => {
-    let path = "https://api.spokendata.com/v2/";
-    return await axios
-      .request({
-        url: path + endpoint,
-        method: method,
-        headers: {
-          "accept": "application/json",
-          "X-API-KEY": apiToken,
-        },
-        ...config,
-      })
-      .then((r: { data: any; status: any; statusText: any; }) => {
-        return { data: r.data, status: r.status, statusText: r.statusText };
-      })
-      .catch((err: { response: { status: any; data: any; }; }) => {
-        if (!err.response) return { status: "", data: "" };
-        console.error(err.response);
-        return { status: err.response.status, data: err.response.data };
-      });
-  };
+  data: any,
+  endpoint: string,
+  method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE" ) => {
+
+  return await axios.request({
+    url: path + endpoint,
+    method: method,
+    headers: {
+      "accept": "application/json",
+      "X-API-KEY": apiToken,
+    },
+    ...data,
+    })
+    .then((response: { data: any; status: any; statusText: any; }) => {
+      return { data: response.data, status: response.status, statusText: response.statusText };
+    })
+    .catch((error: { response: { status: any; data: any; }; }) => {
+      if (!error.response) {
+        return { status: "", data: "" };
+      }
+      console.error(error.response);
+
+      return { status: error.response.status, data: error.response.data };
+    });
+};
