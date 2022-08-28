4d757446426903180a89630de76a170174477ae0
seaz5150 <jan.plhal1@gmail.com>
2022-08-28 16:03:32 +0200

    Added redux-logger for debug.

--- src/state/Store.ts
+++ src/state/Store.ts
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./ReducersIndex";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 export const store = createStore(
     reducers,
     {},
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, logger)
 )
