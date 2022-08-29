46522b9ef069ce4e6206d7da3e6f339a72e8c0cd
seaz5150 <jan.plhal1@gmail.com>
2022-08-29 10:51:16 +0200

    Removed redux-logger.

--- src/state/Store.ts
+++ src/state/Store.ts
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./ReducersIndex";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 
 export const store = createStore(
     reducers,
     {},
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
 )
